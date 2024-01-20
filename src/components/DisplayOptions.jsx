c053c935902ba2781ed832df605fd9502f35df81
Dev <devbajaj.bits@gmail.com>
2024-01-20 16:41:31 +0530

    .

--- src/components/DisplayOptions.jsx
+++ src/components/DisplayOptions.jsx
@@ -1,6 +1,26 @@
-const DisplayOptions = () =>{
-    return(<div>
+import React from 'react';
 
+const DisplayOptions = ({ grouping, setGrouping, sort, setSort }) => {
+    const handleGroupingChange = (e) => setGrouping(e.target.value);
+    const handleSortChange = (e) => setSort(e.target.value);
+
+    return (
+    <div className='display-options'>
+        <label>
+            Group By:
+            <select value={grouping} onChange={handleGroupingChange}>
+                <option value="status">Status</option>
+                <option value="user">User</option>
+                <option value="priority">Priority</option>
+            </select>
+        </label>
+        <label>
+            Sort By:
+            <select value={sort} onChange={handleSortChange}>
+                <option value="priority">Priority</option>
+                <option value="title">Title</option>
+            </select>
+        </label>
     </div>);
 }
 
